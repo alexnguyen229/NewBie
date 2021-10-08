0776c3f8620b1cd8c7cb0de42244cc0e8f38cf64
Alex <alexnguyen123229@gmail.com>
2021-10-08 21:49:31 +0700

    sss

--- getAPI.js
+++ getAPI.js
@@ -14,14 +14,12 @@ async function link_shorter() {
     } = data;
 
 
+    
+    domain1.innerHTML = result.full_short_link;
 
-    console.log(result);
-    domain1.innerHTML = result.short_link;
-    domain1.href = result.full_short_link;
+    
+    domain2.innerHTML = result.full_short_link2;
 
-    domain2.innerHTML = result.short_link2;
-    domain2.href = result.full_short_link2;
-
-    domain3.innerHTML = result.short_link3;
-    domain3.href = result.full_short_link3;
+   
+    domain3.innerHTML = result.full_short_link3;
 }
